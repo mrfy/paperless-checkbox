24cdca6c07b795ae20fd3a6d25b0834d64d7324c
grzegorz fydrych <fydrych.grzegorz.ext@nokia.com>
2021-07-05 21:06:31 +0200

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
+import App from "./App.vue";
+import CompositionApi from "@vue/composition-api";
+import Vue from "vue";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
+Vue.use(CompositionApi);
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
